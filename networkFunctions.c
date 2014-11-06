6bd2dcd2565cd0e2114e318445870ab7e0bcfbe5
sdrans <sebastien@drans.eu>
2014-11-06 21:25:22 +0100

    Ajout de deux fonction "réseaux"
    Ajout de la fonction de création des sockets. Création de la fonction
    contenant la boucle principale du serveur (possibilité d'ouvrir
    plusieurs ports TCP & UDP).

--- networkFunctions.c
+++ networkFunctions.c
@@ -1,1 +1,102 @@
-//Implémentation des fonctions usuelles réseaux (ouverture de connexion, envoie de données....)
+#include "networkFunctions.h"
+#define ATTENTE_MAX 5
+
+/***************************
+		CreerSocket
+***************************/
+int creerSocket(u_short port, int type) {
+    int sock;
+    int retour;
+    struct sockaddr_in adresse;
+
+    if( (sock = socket(AF_INET, type, 0)) < 0) {
+        perror("Impossible d'ouvrir la socket\n");
+        return -1;
+    }
+
+    adresse.sin_family      = AF_INET;
+    adresse.sin_port        = htons(port);
+    adresse.sin_addr.s_addr = INADDR_ANY;
+
+    if( (retour = bind(sock, (struct sockaddr *) &adresse, sizeof adresse)) < 0) {
+        perror("Nommage de la socket impossible\n");
+        return -1;
+    }
+
+    return sock;
+}
+
+/***************************
+		serverLoop
+***************************/
+int serverLoop(u_short nbSocketsTCP, u_short nbSocketsUDP, u_short portInitial) {
+    fd_set readFds;
+    int descripteursSockets[nbSocketsTCP + nbSocketsUDP];
+    int i;
+    int nbSockets = nbSocketsTCP + nbSocketsUDP;
+
+    //Initialisation des sockets
+    for(i = 0; i < nbSocketsTCP; i++) {
+        if( (descripteursSockets[i] = creerSocket(portInitial + i, SOCK_STREAM)) < 0) {
+            perror("Erreur de création de socket TCP\n");
+            return -1;
+        }
+        printf("Socket TCP n° %d creee sur le port %d\n", i, i + portInitial);
+    }
+
+    for(; i < nbSockets; i++) {
+        if( (descripteursSockets[i] = creerSocket(portInitial + i, SOCK_DGRAM)) < 0 ) {
+            perror("Erreur de création de socket UDP\n");
+            return -1;
+        }
+        printf("Socket UDP n° %d creee sur le port %d\n", i, i + portInitial);
+    }
+    //Fin initialisation des sockets
+
+    //Demarrage des sockets
+    for(i = 0; i < nbSockets; i++) {
+        listen(descripteursSockets[i], ATTENTE_MAX);
+        printf("Socket n° %d placee en mode ecoute\n", i);
+    }
+
+    //Boucle principale serveur
+    while(1) {
+        //Initialisation de l'ensemble des descripteurs à lire
+        FD_ZERO(&readFds);
+        printf("FD_ZERO\n");
+        for(i = 0; i < nbSockets; i++) {
+            printf("FD_SET %d\n", i);
+            FD_SET(descripteursSockets[i], &readFds);
+        }
+        //Fin initialisation des descripteurs à lire
+
+        if(select(descripteursSockets[nbSockets - 1] + 1, &readFds, 0, 0, 0) < 0) {
+            perror("Erreur de lecture des descripteurs\n");
+            break; //En cas d'erreur on quitte la boucle pour fermer les sockets
+        }
+
+        for(i = 0; i < nbSockets; i++) {
+            printf("IS SET %d?\n", i);
+            sleep(1);
+            if(FD_ISSET(descripteursSockets[i], &readFds)) {
+                if(i < nbSocketsTCP) {
+                    printf("Requete TCP arrivée sur la socket n°%d\n", i);
+                    //TODO traitement requete TCP
+                }
+                else {
+                    printf("Requete UDP arrivée sur la socket n°%d\n", i);
+                    //TODO traitement requete UDP
+                }
+            }
+        }
+
+    }
+    //Fin boucle principale serveur
+
+    //Fermeture des sockets
+    for(i = 0; i < nbSockets; i++) {
+        close(descripteursSockets[i]);
+        printf("Socket n°%d fermee\n", i);
+    }
+    //Fin fermeture des sockets
+}
--- networkFunctions.h
+++ networkFunctions.h
@@ -1,1 +1,60 @@
-//En-têtes des fonctions usuelles réseaux (ouverture de connexion, envoie de données....)
+/*! \file networkFunctions.h
+\brief fonctions usuelles reseaux (ouverture de connexion, envoie de donnees....)
+*/
+#ifndef NETWORK_FUNCTIONS
+#define NETWORK_FUNCTIONS
+#include <stdio.h>          // Fichier contenant les en-têtes des fonctions standard d'entrées/sorties
+#include <stdlib.h>         // Fichier contenant les en-têtes de fonctions standard telles que malloc()
+#include <string.h>         // Fichier contenant les en-têtes de fonctions standard de gestion de chaînes de caractères
+#include <unistd.h>         // Fichier d'en-têtes de fonctions de la norme POSIX (dont gestion des fichiers : write(), close(), ...)
+#include <sys/types.h>      // Fichier d'en-têtes contenant la définition de plusieurs types et de structures primitifs (système)
+#include <sys/socket.h>     // Fichier d'en-têtes des fonctions de gestion de sockets
+#include <netinet/in.h>     // Fichier contenant différentes macros et constantes facilitant l'utilisation du protocole IP
+#include <netdb.h>          // Fichier d'en-têtes contenant la définition de fonctions et de structures permettant d'obtenir des informations sur le réseau (gethostbyname(), struct hostent, ...)
+#include <memory.h>         // Contient l'inclusion de string.h (s'il n'est pas déjà inclus) et de features.h
+#include <errno.h>          // Fichier d'en-têtes pour la gestion des erreurs (notamment perror())
+
+/**
+* Créer une socket liee au port de numero "port"
+* @param port : le numero du port auquel sera liee la socket
+* @param typeSocket : type de la socket a creer (generalement SOCK_STREAM [TCP] ou SOCK_DGRAM [UDP])
+*
+* @return Retourne le descripteur de la socket (>0) si la creation s'est passe correctement, -1 sinon
+*/
+int creerSocket(u_short port, int typeSocket);
+
+
+/**
+* Traite une requete TCP arrivee sur la socket pointee par le pointeur "socket"
+* @param socket : le pointeur pointant la socket sur laquelle est arrivee la requete
+*
+* @return Retourne 0 si le traitement de la requete s'est passe correctement, -1 sinon
+*/
+//int traiterRequeteTCP(int* socket);
+
+/**
+* Traite une requete UDP arrivee sur la socket pointee par le pointeur "socket"
+* @param socket : le pointeur pointant la socket sur laquelle est arrivee la requete
+*
+* @return Retourne 0 si le traitement de la requete s'est passe correctement, -1 sinon
+*/
+//int traiterRequeteUDP(int* socket);
+
+/**
+* Fonction d'intialistion du serveur :
+* Premiere etape :
+* Creation des sockets TCP et UDP a partir du numero de port "portInitial"
+* Ainsi si l'on souhaite creer n sockets TCP et m sockets UDP a partir du port k, les sockets TCP seront ouvertes sur les ports [k..k+n] et les sockets UDP sur les ports [k+n+1..k+n+1+m]
+*
+* Deuxieme etape :
+* La fonction initialise la boucle principale du serveur qui repondra aux requetes arrivant sur les sockets à l'aide des fonction de traitements TCP et UDP
+*
+* @param nbSocketsTCP : nombre de sockets TCP a ouvrir
+* @param nbSocketsUDP : nombre de sockets UDP a ouvrir
+* @param portInitial : numero de port a partir duquel les sockets seront initialisees
+*
+* @return Retourne -1 si une erreur survient
+*/
+int serverLoop(u_short nbSocketsTCP, u_short nbSocketsUDP, u_short portInitial);
+
+#endif
